c55fd69a67a9507b44980f12d317aa730298cb50
iiiz <dcathers1@gmail.com>
2026-02-22 15:16:41 -0600

    fix: validation preventing issues on partial monitor config and sane defaults.

--- lib/config.go
+++ lib/config.go
@@ -4,18 +4,23 @@ import (
 	"encoding/json"
 	"os"
 	"path/filepath"
+
+	"github.com/charmbracelet/log"
+	"gopkg.in/go-playground/validator.v8"
 )
 
 type MonitorConfig struct {
-	Id          string  `json:"id"`
-	DefaultName *string `json:"defaultName,omitempty"`
+	Id          string  `validate:"required" json:"id"`
+	DefaultName *string `validate:"required,max=255" json:"defaultName"`
 }
 
 type Config struct {
-	Monitors     []MonitorConfig `json:"monitors,omitempty"`
-	AutoComplete []string        `json:"autoComplete,omitempty"`
-	PrimaryName  *string         `json:"primaryName,omitempty"`
+	Monitors     []MonitorConfig `validate:"omitempty,dive" json:"monitors,omitempty"`
+	AutoComplete []string        `validate:"omitempty,max=100" json:"autoComplete,omitempty"`
+	PrimaryName  *string         `validate:"omitempty,min=1,max=255" json:"primaryName,omitempty"`
 }
+
+var validate *validator.Validate
 
 func ReadConfig() *Config {
 	homeDir, err := os.UserHomeDir()
@@ -28,10 +33,17 @@ func ReadConfig() *Config {
 		Check(err)
 		defer file.Close()
 
+		validatiorConfig := &validator.Config{TagName: "validate"}
+		validate = validator.New(validatiorConfig)
+
 		var config Config
 
 		err = json.NewDecoder(file).Decode(&config)
 		Check(err)
+
+		if err := validate.Struct(&config); err != nil {
+			log.Fatal(err)
+		}
 
 		return &config
 	}
